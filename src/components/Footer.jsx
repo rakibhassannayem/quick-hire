db27255327a915d032a8a7f63cb30771b8950632
rakibhassannayem <rakibhassannayem@gmail.com>
2026-03-04 12:16:58 +0600

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Logo from './shared/Logo';
 import Container from './shared/Container';
 import Link from 'next/link';
+import { RiFacebookFill, RiInstagramLine, RiLinkedinFill, RiTwitterFill } from "react-icons/ri";
+import { BsGlobe2 } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -48,6 +48,34 @@ const Footer = () => {
             </form>
           </nav>
         </div>
+        <hr className='my-4 border-gray-500' />
+        <footer className="footer sm:footer-horizontal text-neutral-content items-center py-4">
+          <aside className="grid-flow-col items-center text-gray-400">
+            <p>{new Date().getFullYear()} @ QuicHire. All rights reserved.</p>
+          </aside>
+          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a className='bg-gray-600 rounded-full p-1.5'>
+              <RiFacebookFill size={20} />
+            </a>
+
+            <a className='bg-gray-600 rounded-full p-1.5'>
+              <RiInstagramLine size={20} />
+            </a>
+
+            <a className='bg-gray-600 rounded-full p-1.5'>
+              <BsGlobe2 size={20} />
+            </a>
+
+            <a className='bg-gray-600 rounded-full p-1.5'>
+              <RiLinkedinFill size={20} />
+            </a>
+
+            <a className='bg-gray-600 rounded-full p-1.5'>
+              <RiTwitterFill size={20} />
+            </a>
+            
+          </nav>
+        </footer>
       </Container>
 
 
